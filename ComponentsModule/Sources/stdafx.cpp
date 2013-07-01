1d4ec73b4b69c120ffd6a595f5870a014c6e8ebd
Ivan Sialiuk <sialiuk@gmail.com>
2013-07-02 00:41:41 +0300

    Refactoring structure of Chat solution.

--- ComponentsModule/Sources/stdafx.cpp
+++ ComponentsModule/Sources/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// HlpModule.pch will be the pre-compiled header
+// ComponentsModule.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
--- HelpersModule/Sources/stdafx.cpp
+++ HelpersModule/Sources/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// LoginModule.pch will be the pre-compiled header
+// HelpersModule.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
